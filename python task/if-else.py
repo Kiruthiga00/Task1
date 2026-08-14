16704f08f72cddaab6accee6271a666f5cd4f0c5
Kiruthiga00 <keerthi5102004@gmail.com>
2026-08-14 20:34:57 +0530

    add the python file

---
+++ python task/if-else.py
@@ -0,0 +1,93 @@
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# a = 50
+# b = 70
+# if a > b:
+#     print("Largest:", a)
+# else:
+#     print("Largest:", b)
+
+
+# num = int(input("Enter a number: "))
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible to vote")
+
+
+# year = int(input("Enter a year: "))
+# if (year % 4 == 0):
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
+
+
+# ch = input("Enter a character: ")
+# if ch in "aeiouAEIOU":
+#     print("Vowel")
+# else:
+#     print("Consonant")
+
+
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("Child")
+# elif age < 20:
+#     print("Teenager")
+# else:
+#     print("Adult")
+
+
+# day = int(input("Enter day number (1-7): "))
+# if day == 1:
+#     print("Monday")
+# elif day == 2:
+#     print("Tuesday")
+# elif day == 3:
+#     print("Wednesday")
+# elif day == 4:
+#     print("Thursday")
+# elif day == 5:
+#     print("Friday")
+# elif day == 6:
+#     print("Saturday")
+# elif day == 7:
+#     print("Sunday")
+# else:
+#     print("Invalid day number")
+
+
+# score=int(input("Enter a score:"))
+# if(score>75):
+#     print("Good Student")
+# elif(score>35 & score<75):
+#     print("Average Student")
+# else:
+#     print("Poor Student")
+
+
+# score=int(input("Score Percentage:"))
+# if(score>=70):
+#     name=input("name:")
+#     department=input("department:")
+#     location=input("location:")
+#     print("you are eligible")
+# else:
+#     print("you are not eligible")
+
+
+# 
