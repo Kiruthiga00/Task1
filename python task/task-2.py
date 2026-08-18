3e91fbdc732f76b6f2ba56998c5a87ea8bdf9b20
Kiruthiga00 <keerthi5102004@gmail.com>
2026-08-18 17:34:20 +0530

    save the file

---
+++ python task/task-2.py
@@ -0,0 +1,43 @@
+# All divisor
+    # no = 1029
+    # div = 2
+    # while div < no:
+    #     if no % div == 0:
+    #         print(div)         
+    #     div+=1
+
+# first divisor
+# no = 1029
+# div = 2
+# while div < no:
+#         if no % div == 0:
+#             print(div)
+#             break
+#          div+=1
+
+# last divisor
+# no = 1029
+# div = 2
+# last_div = 0
+# while div < no:
+#     if no % div == 0:
+#         print(div)   
+#         last_div = div        
+#     div+=1
+
+# print("last div:" ,last_div)
+
+# two digit divisor 
+# no = 1029
+# div = 10
+# while div < 100:
+#     if no % div == 0:
+#         print(div)
+#     div+=1
+
+# no = 1029
+# div = 10
+# while div < 100:
+#     if no % div == 0:
+#         print(div//10)     
+#     div+=1
