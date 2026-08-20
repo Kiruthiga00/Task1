be76a3d525be886cb35d5858ddf154c98d3360de
Kiruthiga00 <keerthi5102004@gmail.com>
2026-08-20 20:32:06 +0530

    save the file

---
+++ python task/task-4.py
@@ -0,0 +1,29 @@
+#  Prime Number
+# n=int(input('Enter a number: '))
+# div = 2
+# while div < n: 
+#     if n % div == 0:   
+#         print('Not a Prime Number')
+#         break
+#     div+=1
+# else:
+#     print('Prime Number')
+
+
+# check factorial
+# n=int(input('Enter a number: '))
+# total = 0
+# factor = 1
+# while factor < n:
+#     if n % factor == 0:
+#         total = total + factor
+#     factor += 1
+#     if total == n:
+#         print("Perfect Number")
+#         break
+# else:
+#     print("Not a Perfect Number")
+
+
+
+
